5b0e20aa712ab3982b1cec88278b552253358209
Puria Rad <puriajahanbani@gmail.com>
2022-01-24 23:23:04 +0330

    product added

--- frontend/src/app/classes/Product.ts
+++ frontend/src/app/classes/Product.ts
@@ -1,1 +1,19 @@
-export class Product { }
+export class Product {
+  constructor(
+    private _id: number,
+    public name: string,
+    public description: string,
+    public defaultImage: string,
+    public images: string[],
+    public price: number,
+    public discount: number
+  ) {}
+
+  get id() {
+    return this._id;
+  }
+
+  get finalPrice() {
+    return this.price - this.discount >= 0 ? this.price - this.discount : 0;
+  }
+}
