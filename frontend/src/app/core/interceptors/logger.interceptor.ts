ef40bb3d4c1342b2f80032afd56de4bc876467fa
Puria Rad <puriajahanbani@gmail.com>
2022-01-24 22:02:31 +0330

    add core module

---
+++ frontend/src/app/core/interceptors/logger.interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoggerInterceptor implements HttpInterceptor {
+  constructor() { }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req);
+  }
+}
