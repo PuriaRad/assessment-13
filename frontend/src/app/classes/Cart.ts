31c5c09603aa37004417a86fd9a6a073765c9596
Puria Rad <puriajahanbani@gmail.com>
2022-01-24 23:23:22 +0330

    cart added

--- frontend/src/app/classes/Cart.ts
+++ frontend/src/app/classes/Cart.ts
@@ -1,1 +1,31 @@
-export class Cart { }
+import { Product } from './product';
+
+export class Cart {
+  constructor(
+    private _id: number,
+    public productOrders: {
+      product: Product;
+      quantity: number;
+    }[]
+  ) {}
+
+  get id() {
+    return this._id;
+  }
+
+  get sumDiscount() {
+    return this.productOrders.map((productOrder) => productOrder.product.discount * productOrder.quantity).reduce((p, c) => p + c, 0);
+  }
+
+  get sumPrice() {
+    return this.productOrders
+      .map((productOrder) => productOrder.product.price * productOrder.quantity)
+      .reduce((p, c) => p + c, 0);
+  }
+
+  get sumFinalPrice() {
+    return this.productOrders
+      .map((productOrder) => productOrder.product.finalPrice * productOrder.quantity)
+      .reduce((p, c) => p + c, 0);
+  }
+}
