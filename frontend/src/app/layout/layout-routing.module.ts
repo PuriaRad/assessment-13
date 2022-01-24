13258604e6a808f4096756b719f849fd5ae6ea19
Puria Rad <puriajahanbani@gmail.com>
2022-01-24 22:34:06 +0330

    setup layout

--- frontend/src/app/layout/layout-routing.module.ts
+++ frontend/src/app/layout/layout-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorizationGuard } from '../core/guards/authorization.guard';
 import { LayoutComponent } from './layout.component';
 
-const routes: Routes = [{ path: '', component: LayoutComponent }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'product', loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule) }, { path: 'admin-product', loadChildren: () => import('./pages/admin-product/admin-product.module').then(m => m.AdminProductModule) }, { path: 'admin-products', loadChildren: () => import('./pages/admin-products/admin-products.module').then(m => m.AdminProductsModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule) },
+      {
+        path: 'product',
+        loadChildren: () => import('./pages/product/product.module').then((m) => m.ProductModule),
+      },
+      {
+        path: 'admin-product',
+        loadChildren: () =>
+          import('./pages/admin-product/admin-product.module').then((m) => m.AdminProductModule),
+        canActivate: [AuthorizationGuard],
+      },
+      {
+        path: 'admin-products',
+        loadChildren: () =>
+          import('./pages/admin-products/admin-products.module').then((m) => m.AdminProductsModule),
+        canActivate: [AuthorizationGuard],
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LayoutRoutingModule { }
+export class LayoutRoutingModule {}
