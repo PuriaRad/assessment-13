dad5faf56176fd04ed8b15d907bbccd3ef2c0796
puriarad <puriajahanbani@gmail.com>
2022-01-25 12:11:24 +0330

    user get all added to user api

--- frontend/src/app/core/api/custom/user-api.service.ts
+++ frontend/src/app/core/api/custom/user-api.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import {
+  map,
+  Observable,
+} from 'rxjs';
+import { User } from 'src/app/classes/user';
+
+import { CrudService } from '../crud.service';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserApiService {
+  constructor(private crud: CrudService) {}
 
-  constructor() { }
+  getAllUsers(): Observable<User[]> {
+    return this.crud
+      .getAll<User>('users')
+      .pipe(
+        map((users) =>
+          users.map(
+            (user) =>
+              new User(user.id, user.name, user.phone, user.avatar, user.email, user.address, user.role)
+          )
+        )
+      );
+  }
 }
