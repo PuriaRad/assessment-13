690c7c98a87b4d8c2806d40c41915976f8fa41a6
puriarad <puriajahanbani@gmail.com>
2022-01-25 12:10:33 +0330

    core module added

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,12 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  ErrorHandler,
+  ModuleWithProviders,
+  NgModule,
+  Optional,
+  SkipSelf,
+} from '@angular/core';
 
-
+import { ErrorHandlerService } from './error/error-handler.service';
+import { httpInterceptorProviders } from './interceptors';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, HttpClientModule],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
+  static forRoot(): ModuleWithProviders<CoreModule> {
+    return {
+      ngModule: CoreModule,
+      providers: [httpInterceptorProviders, { provide: ErrorHandler, useClass: ErrorHandlerService }],
+    };
+  }
+}
