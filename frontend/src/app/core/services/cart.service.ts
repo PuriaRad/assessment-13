9073cb0c3141512b7b9fe542299f2332dedfa5bf
puriarad <puriajahanbani@gmail.com>
2022-01-25 11:18:52 +0330

    cart waiting for API service

--- frontend/src/app/core/services/cart.service.ts
+++ frontend/src/app/core/services/cart.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+import { BehaviorSubject } from 'rxjs';
+import { Cart } from 'src/app/classes/cart';
+import { Product } from 'src/app/classes/product';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  cart = new BehaviorSubject<Cart | null>(null);
+  constructor() {}
 
-  constructor() { }
+  setCart(userId:number):void{
+    //TODO Call api and get the cart. Then set it to this.cart
+  }
+
+  addProduct(product: Product, quantity: number):void {
+    if (this.cart.getValue() !== null) {
+      const currentValue: Cart = this.cart.getValue() as Cart;
+
+      // check if product is already in the list. If it is, just add the quantity, if not, push new prod to cart
+      const duplicateIndex = currentValue.productOrders.findIndex((prod) => prod.product.id === product.id);
+      if (duplicateIndex) {
+        currentValue.productOrders[duplicateIndex].quantity =
+          currentValue.productOrders[duplicateIndex].quantity + quantity;
+      } else {
+        currentValue.productOrders.push({ product, quantity });
+      }
+
+      this.cart.next(currentValue);
+
+      //TODO call the webservice and update the Cart
+    }
+  }
+
+  // Maybe in the future :D
+  // removeProduct() {
+
+  // }
 }
