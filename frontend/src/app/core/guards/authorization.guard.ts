93c81144eddeccb125cddc8ca182a2f6ace21b9f
Puria Rad <puriajahanbani@gmail.com>
2022-01-25 22:47:38 +0330

    authorization added

--- frontend/src/app/core/guards/authorization.guard.ts
+++ frontend/src/app/core/guards/authorization.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthorizationGuard implements CanActivate {
+  constructor(private router: Router, public userService: UserService) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.userService.user.pipe(
+      map((user) => {
+        if (user?.role == 'ADMIN') {
+          return true;
+        } else {
+          this.router.navigate(['/auth']);
+          return false;
+        }
+      })
+    );
   }
-  
 }
