d8d13af5708d807737c7f7c181d92e455fb6d16e
puriarad <puriajahanbani@gmail.com>
2022-01-25 16:42:55 +0330

    home added

--- frontend/src/app/layout/pages/home/home.component.ts
+++ frontend/src/app/layout/pages/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/classes/product';
+import { ProductApiService } from 'src/app/core/api/custom/product-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  recoms$!: Observable<Product[]>;
+  constructor(private productApi: ProductApiService) {}
 
   ngOnInit(): void {
+    this.productApi.getRecommended().pipe((recoms) => (this.recoms$ = recoms));
   }
 
+  search(event: Event) {}
 }
