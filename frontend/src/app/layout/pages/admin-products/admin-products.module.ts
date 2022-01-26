723124942e18e3d25ae1b9b364dfcd7a8a1bfe2d
puriarad <puriajahanbani@gmail.com>
2022-01-26 10:43:00 +0330

    admin products finalized

--- frontend/src/app/layout/pages/admin-products/admin-products.module.ts
+++ frontend/src/app/layout/pages/admin-products/admin-products.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+import { EButtonModule } from 'src/app/elements/e-button/e-button.module';
 
 import { AdminProductsRoutingModule } from './admin-products-routing.module';
 import { AdminProductsComponent } from './admin-products.component';
 
-
 @NgModule({
-  declarations: [
-    AdminProductsComponent
-  ],
+  declarations: [AdminProductsComponent],
   imports: [
     CommonModule,
-    AdminProductsRoutingModule
-  ]
+    AdminProductsRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    EButtonModule,
+    MatButtonModule,
+  ],
 })
-export class AdminProductsModule { }
+export class AdminProductsModule {}
