97aafc4f6b8a6ddcf5f0b6fbca64de0248480357
puriarad <puriajahanbani@gmail.com>
2022-01-26 11:27:03 +0330

    added e input

--- frontend/src/app/elements/e-input/e-input.component.ts
+++ frontend/src/app/elements/e-input/e-input.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  Renderer2,
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-e-input',
   templateUrl: './e-input.component.html',
-  styleUrls: ['./e-input.component.scss']
+  styleUrls: ['./e-input.component.scss'],
 })
 export class EInputComponent implements OnInit {
+  @Input() public label!: string;
+  @Input() public required = false;
+  @Input() public startIcon!: string; // start icon
 
-  constructor() { }
+  @Output() public inputEvent = new EventEmitter(); // ending icon click event
+
+  @Input() public col = '12'; // mobile
+  @Input() public sm = '12'; // small
+  @Input() public md = '6'; // medium
+  @Input() public lg = '4'; // medium
+  @Input() public xl = '3'; // extra large
+  @Input() public hasMy3 = true; // my-3
+  @Input() public zeroPadding = false; // p-0
+
+  @Input() public formGroup!: FormGroup;
+  @Input() public controlName!: string;
+  @Input() public control!: any;
+  @Input() public autocomplete = 'on';
+  @Input() public type = 'text';
+  @Input() public placeholder = '';
+
+  @Input() public minlength: any;
+  @Input() public maxlength: any;
+  @Input() public pattern: any;
+  @Input() public min: any;
+  @Input() public max: any;
+
+  @Input() public precision = 0;
+  @Input() public suffix = ' $';
+
+  constructor(
+    private cdref: ChangeDetectorRef,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
+    if (this.hasMy3) {
+      this.renderer.addClass(this.elementRef.nativeElement, 'my-3');
+    }
+    if (this.zeroPadding) {
+      this.renderer.addClass(this.elementRef.nativeElement, 'p-0');
+    }
+    this.renderer.addClass(this.elementRef.nativeElement, 'no-outline');
+    this.renderer.addClass(this.elementRef.nativeElement, 'col-' + this.col);
+    this.renderer.addClass(this.elementRef.nativeElement, 'col-sm-' + this.sm);
+    this.renderer.addClass(this.elementRef.nativeElement, 'col-md-' + this.md);
+    this.renderer.addClass(this.elementRef.nativeElement, 'col-lg-' + this.lg);
+    this.renderer.addClass(this.elementRef.nativeElement, 'col-xl-' + this.xl);
   }
 
+  ngAfterContentChecked(): void {
+    this.cdref.detectChanges();
+  }
 }
--- frontend/src/app/elements/e-input/e-input.module.ts
+++ frontend/src/app/elements/e-input/e-input.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+
+import { NgxCurrencyModule } from 'ngx-currency';
+
 import { EInputComponent } from './e-input.component';
 
-
-
 @NgModule({
-  declarations: [
-    EInputComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    EInputComponent
-  ]
+  declarations: [EInputComponent],
+  imports: [CommonModule, MatRippleModule, MatButtonModule, ReactiveFormsModule, NgxCurrencyModule],
+  exports: [EInputComponent],
 })
-export class EInputModule { }
+export class EInputModule {}
